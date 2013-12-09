f7fdf937a8517c0b8cf3411e85116d09011df15d
Szymon Katra <katra.szymon@gmail.com>
2013-12-09 17:42:20 +0100

    readme fix

---
+++ GG4NET/ImageData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GG4NET
+{
+    /// <summary>
+    /// Dane obraza.
+    /// </summary>
+    public struct ImageData
+    {
+        /// <summary>
+        /// Suma kontrolna CRC32.
+        /// </summary>
+        public uint Crc32;
+        /// <summary>
+        /// Wielkość obrazka w bajtach.
+        /// </summary>
+        public uint Length;
+        /// <summary>
+        /// Nazwa pliku obrazka.
+        /// </summary>
+        public string FileName;
+        /// <summary>
+        /// Dane obrazka.
+        /// </summary>
+        public byte[] Data;
+    }
+}
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 ﻿GG4NET
 ======
 
-Biblioteka do obsługi Gadu-Gadu dla .NET bazowana na specyfikacji toxygen'a (http://toxygen.net/libgadu/protocol/).
+Biblioteka do obsługi Gadu-Gadu dla .NET bazowana na specyfikacji libgadu (http://libgadu.net/protocol/).
 
 Darmowe do użytku, wzmianka w autorach nie wymagana ale mile widziana.
 
 ----------
 
-.NET Gadu-Gadu library based on toxygen specification (http://toxygen.net/libgadu/protocol/).
+.NET Gadu-Gadu library based on libgadu specification (http://libgadu.net/protocol/).
 
 Free tu use, credits are not required but will be appreciated.
